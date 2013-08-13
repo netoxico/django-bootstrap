bdf3cef73c871cfd277aa4f9cde21b7b873ba311
Ernesto Vargas <me@netoxico.com>
2013-08-13 13:16:40 -0500

    Fix typo

--- project_name/manage.py
+++ project_name/manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     environment = os.environ.get('PROJECT_ENV', 'development')
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{project_name}}.settings.%s", environment)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{project_name}}.settings.%s" % environment)
 
     from django.core.management import execute_from_command_line
 
