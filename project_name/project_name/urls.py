5a1662dd219b29863dfd366128a4ee958f6079ae
Jesus Castillo <jesuscmd@gmail.com>
2013-08-14 10:48:15 -0500

    added media folder and configured on urls.py and settings

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
@@ -22,4 +23,4 @@ urlpatterns = patterns(
 )
 
 if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
