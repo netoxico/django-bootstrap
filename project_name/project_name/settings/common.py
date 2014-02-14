4a09d8cbb9a15a6f770c03103061b064b29f9797
Ernesto Vargas <me@netoxico.com>
2014-02-13 22:00:33 -0600

    removed south

--- project_name/project_name/settings/common.py
+++ project_name/project_name/settings/common.py
@@ -116,7 +116,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     'compressor',
-    'south',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
