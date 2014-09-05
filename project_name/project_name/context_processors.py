a8fae3bdc8353e289503a87666022a2190fec0ef
Citlalli Murillo <citmusa@hotmail.com>
2014-09-05 10:51:29 -0500

    Create context_processors.py

---
+++ project_name/project_name/context_processors.py
@@ -0,0 +1,11 @@
+#coding: utf-8
+from os import popen
+
+
+def git(request):
+    git_branch = popen('git rev-parse --abbrev-ref HEAD').readline().rstrip()
+    git_commit = popen('git rev-parse HEAD').readline().rstrip()
+    return{
+        'GIT_BRANCH': git_branch,
+        'GIT_COMMIT': git_commit
+    }
